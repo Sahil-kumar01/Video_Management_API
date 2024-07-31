616a1a388679956b1c3bfafbbfed8dd20a818f9c
Sahil-kumar01 <sahilkumar.ktp16@gmail.com>
2024-07-31 10:33:15 +0530

    added a function to check if the uploaded file is video format.

--- app/routers/video.py
+++ app/routers/video.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, File, UploadFile, HTTPException, Form
+from fastapi import APIRouter, File, UploadFile, HTTPException, Form,status
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
 from typing import List, Optional
@@ -6,15 +6,10 @@ from datetime import datetime
 import os
 import shutil
 from app.services.video import save_video_metadata, get_video_by_id, update_video_metadata, delete_video_by_id
-
+from app.models.video import VideoMetadata
 router = APIRouter()
 
 os.makedirs('videos', exist_ok=True)
-
-class VideoMetadata(BaseModel):
-    title: str
-    description: Optional[str] = None
-    tags: Optional[List[str]] = None
 
 @router.post("/upload-video/")
 async def upload_video(
@@ -23,6 +18,14 @@ async def upload_video(
     description: Optional[str] = Form(None),
     tags: List[str] = Form(...)
 ):
+    # check for file format
+    file_types = ["video/mp4", "video/avi", "video/mkv", "video/mov"]
+    if file.content_type not in file_types:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid file format. Please upload a video file."
+        )
+    
     file_path = os.path.join('videos', file.filename)
     
     with open(file_path, "wb") as buffer:
